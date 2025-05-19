3b92c86618c138369402eb19daf85c91a41d1305
Zubkov Andrey <zubkov.andrey.99@mail.ru>
2025-05-19 16:17:49 +0300

    hw-2

--- src/main.jsx
+++ src/main.jsx
@@ -1,36 +1,25 @@
-// import { createElement } from "react";
 import { createRoot } from "react-dom/client";
+import { headphones } from "./constants/mock";
 
 const root = document.getElementById("root");
 
 const reactRoot = createRoot(root);
 
-const data = [1, 2, 3, 4, 5];
-
-// reactRoot.render(
-// createElement(
-//   "ul",
-//   { className: "someClass", style: { color: "red" } },
-//   data.map((el) => createElement("li", {}, createElement("span", {}, el)))
-// )
-
-// createElement(
-//   "button",
-//   {
-//     onClick: () => {
-//       console.log("click");
-//     },
-//   },
-//   "hello world!"
-// )
-// );
-
 reactRoot.render(
-  <ul className='someClass' style={{ color: "red" }}>
-    {data.map((el) => (
-      <li key={el}>{el}</li>
+  <div>
+    <h1>Headphones</h1>
+    {headphones.map(({ name, id, reviews, codecs }) => (
+      <div key={id}>
+        <h2>{name}</h2>
+        <h3>Reviews</h3>
+        {reviews.map((text) => (
+          <span key={text}>{text}</span>
+        ))}
+        <h3>Codecs</h3>
+        {codecs.map((codec) => (
+          <span key={codec}>{codec}</span>
+        ))}
+      </div>
     ))}
-  </ul>
+  </div>
 );
-
-console.log(reactRoot);
